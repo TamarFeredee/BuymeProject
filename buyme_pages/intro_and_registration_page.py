a6e194f580584f01b928064787fcfb587b7383e2
TamarFeredee <94450078+TamarFeredee@users.noreply.github.com>
2023-12-27 09:24:41 +0200

    Update intro_and_registration_page.py

--- buyme_pages/intro_and_registration_page.py
+++ buyme_pages/intro_and_registration_page.py
@@ -11,28 +11,21 @@ class IntroPage(BasePage):
         BasePage.__init__(self, driver)
 
     def click_signin_area(self):
-        time.sleep(3)
         self.click_element(By.CLASS_NAME, 'notSigned')
 
     def click_signin_button(self):
-        time.sleep(3)
         self.click_element(By.CSS_SELECTOR, 'span[aria-label=להרשמה]')
 
     def fill_registration_details(self, first_name, email, password, password_confirmation):
-        time.sleep(3)
         expected_name = first_name
         element = self.enter_text(By.ID, 'ember1917', first_name)
-        time.sleep(3)
         assert expected_name == element.get_attribute()
         self.enter_text(By.ID, 'ember1924', email)
         self.enter_text(By.ID, 'valPass', password)
         self.enter_text(By.ID, 'ember1938', password_confirmation)
-        time.sleep(3)
 
     def click_on_terms_and_conditions_checkbox_button(self):
-        time.sleep(3)
         self.click_element(By.ID, 'ember1944')
 
     def click_registration_button(self):
-        time.sleep(3)
         self.click_element(By.ID, 'ember1948')
